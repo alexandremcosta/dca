d7ca370182c9156244e92aa2a64c36984ba811d0
Alexandre Costa <alexandremcost@gmail.com>
2024-10-12 12:22:25 -0700

    Refactor strategy code and add TODO to readme

--- lib/dollar_cost_avg/strategy.ex
+++ lib/dollar_cost_avg/strategy.ex
@@ -1,45 +1,59 @@
 defmodule DollarCostAvg.Strategy do
   def determine_strategy(ticker, threshold_low, threshold_high) do
-    current_time = DateTime.utc_now()
-    one_year_ago = DateTime.add(current_time, -365 * 24 * 60 * 60)
+    url = build_url(ticker)
 
-    current_time = DateTime.to_unix(current_time)
-    one_year_ago = DateTime.to_unix(one_year_ago)
+    with {:ok, market_data} <- fetch_market_data(url) do
+      {:ok, calculate_strategy(market_data, threshold_low, threshold_high, ticker, url)}
+    end
+  end
+
+  defp build_url(ticker) do
+    current_time = DateTime.utc_now() |> DateTime.to_unix()
+    one_year_ago = DateTime.add(DateTime.utc_now(), -365 * 24 * 60 * 60) |> DateTime.to_unix()
 
     path = "?period2=#{current_time}&period1=#{one_year_ago}&interval=1d"
-    url = "https://query1.finance.yahoo.com/v8/finance/chart/#{ticker}" <> path
+    "https://query1.finance.yahoo.com/v8/finance/chart/#{ticker}" <> path
+  end
 
+  defp fetch_market_data(url) do
     with {:ok, response} <- url |> URI.encode() |> Req.get(req_options()),
-         response = response.body,
-         [first_result | _] <- response["chart"]["result"],
+         response_body = response.body,
+         [first_result | _] <- response_body["chart"]["result"],
          [first_indicator | _] <- first_result["indicators"]["quote"] do
-      high_52_week = Enum.max(first_indicator["high"])
-      daily_high = first_result["meta"]["regularMarketDayHigh"]
+      day_high = first_result["meta"]["regularMarketDayHigh"]
+      year_high = Enum.max(first_indicator["high"])
 
-      threshold_aggressive = high_52_week * threshold_low
-      threshold_normal = high_52_week * threshold_high
-
-      {strategy, color} =
-        cond do
-          daily_high < threshold_aggressive -> {"Buy aggressively", "blue"}
-          daily_high < threshold_normal -> {"Buy normally", "green"}
-          true -> {"Don't buy", "gray"}
-        end
-
-      {:ok,
-       %{
-         ticker: ticker,
-         daily_high: float_to_dollar(daily_high),
-         high_52_week: float_to_dollar(high_52_week),
-         threshold_aggressive: float_to_dollar(threshold_aggressive),
-         threshold_normal: float_to_dollar(threshold_normal),
-         strategy: strategy,
-         color: color,
-         url: url
-       }}
+      {:ok, %{day_high: day_high, year_high: year_high}}
     else
       {:error, request_error} -> {:error, request_error}
-      nil -> {:error, "not found"}
+      _empty_or_nil -> {:error, "not found"}
+    end
+  end
+
+  defp calculate_strategy(market_data, threshold_low, threshold_high, ticker, url) do
+    threshold_aggressive = market_data.year_high * threshold_low
+    threshold_normal = market_data.year_high * threshold_high
+
+    {strategy, color} =
+      determine_buy_strategy(market_data.day_high, threshold_aggressive, threshold_normal)
+
+    %{
+      ticker: ticker,
+      daily_high: float_to_dollar(market_data.day_high),
+      high_52_week: float_to_dollar(market_data.year_high),
+      threshold_aggressive: float_to_dollar(threshold_aggressive),
+      threshold_normal: float_to_dollar(threshold_normal),
+      strategy: strategy,
+      color: color,
+      url: url
+    }
+  end
+
+  defp determine_buy_strategy(daily_high, threshold_aggressive, threshold_normal) do
+    cond do
+      daily_high < threshold_aggressive -> {"Buy aggressively", "blue"}
+      daily_high < threshold_normal -> {"Buy normally", "green"}
+      true -> {"Don't buy", "gray"}
     end
   end
 
@@ -48,23 +62,17 @@ defmodule DollarCostAvg.Strategy do
   end
 
   defp req_options do
+    case get_proxy() do
+      nil -> []
+      proxy -> [connect_options: [proxy: proxy]]
+    end
+  end
+
+  defp get_proxy do
     https_proxy = System.get_env("HTTPS_PROXY")
     http_proxy = System.get_env("HTTP_PROXY")
 
-    proxy =
-      cond do
-        https_proxy ->
-          parse_proxy(https_proxy)
-
-        http_proxy ->
-          parse_proxy(http_proxy)
-
-        true ->
-          nil
-      end
-
-    options = if proxy, do: [proxy: proxy], else: []
-    [connect_options: options]
+    (https_proxy && parse_proxy(https_proxy)) || (http_proxy && parse_proxy(http_proxy))
   end
 
   defp parse_proxy(proxy_url) do
